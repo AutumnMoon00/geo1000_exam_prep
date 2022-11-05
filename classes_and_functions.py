256169341da5969358cf9ae2da84540407c790bd
AutumnMoon00 <s.c.madanu@student.tudelft.nl>
2022-11-05 22:11:35 +0100

    ex 15.1 add time

--- classes_and_functions.py
+++ classes_and_functions.py
@@ -21,11 +21,11 @@ def is_after(t1, t2):
 
 
 def add_time(t1, t2):
-sum = Time()
-sum.hour = t1.hour + t2.hour
-sum.minute = t1.minute + t2.minute
-sum.second = t1.second + t2.second
-return sum
+    sum = Time()
+    sum.hour = t1.hour + t2.hour
+    sum.minute = t1.minute + t2.minute
+    sum.second = t1.second + t2.second
+    return sum
 
 
 noon_time = Time()
