0f60cd609e2075a50f42b6f6f69a0e7987ecfd20
AutumnMoon00 <s.c.madanu@student.tudelft.nl>
2022-11-05 21:48:08 +0100

    ex 15.1 defining Circle class

---
+++ 15.1.py
@@ -0,0 +1,14 @@
+import math
+
+
+class Point:
+    """
+    Represents a point in a 2D space
+    """
+
+
+class Circle:
+    """
+    attributes: center, radius
+    """
+
--- classes_and_objects.py
+++ classes_and_objects.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 
 class Point:
@@ -23,6 +24,18 @@ class Rectangle:
     attributes: width, height, corner
     """
 
+# 15.1
+class Circle:
+    """
+    attributes: center, radius
+    """
+
+
+def point_in_circle(c, p):
+    if distance_between_points(c.center, p) <= c.radius:
+        return True
+    else:
+        return False
 
 def find_center(box):
     center = Point()
@@ -31,18 +44,50 @@ def find_center(box):
     return center
 
 
-box = Rectangle()
-box.width = 100.0
-box.height = 200.0
-box.corner = Point()
-box.corner.x = 0.0
-box.corner.y = 0.0
+def grow_rectangle(rect, dwidth, dheight):
+    rect.width += dwidth
+    rect.height += dheight
 
 
-blank = Point()
-blank.x = 3.0
-blank.y = 4.0
+def move_rectangle(rect, dx, dy):
+    rect.corner.x += dx
+    rect.corner.y += dy
 
 
-center = find_center(box)
-print_point(center)
+def move_rectangle_copy(rect, dx, dy):
+    rect_copy = copy.deepcopy(rect)
+    move_rectangle(rect_copy, dx, dy)
+    return rect_copy
+
+
+def main():
+    box = Rectangle()
+    box.width = 100.0
+    box.height = 200.0
+    box.corner = Point()
+    box.corner.x = 0.0
+    box.corner.y = 0.0
+
+
+    blank = Point()
+    blank.x = 3.0
+    blank.y = 4.0
+
+
+    center = find_center(box)
+    print_point(center)
+
+    print(box.width, box.height)
+    grow_rectangle(box, 50, 100)
+    print(box.width, box.height)
+
+    p1 = Point()
+    p1.x = 3.0
+    p1.y = 4.0
+
+    p2 = copy.deepcopy(p1)
+    print(p2 == p1)
+
+
+if __name__ == '__main__':
+    main()
